459d477517b410480c78689ab24c4f3df34401bd
varadavaidya <60335484+varadavaidya@users.noreply.github.com>
2025-08-14 23:27:41 +0530

    Add files via upload

---
+++ traffic-generator/traffic-gen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# example ./traffic-gen.sh www.google.com 2
+
+if [ "$#" -lt 2 ]; then 
+    echo "Usage:$0 <target> <interval-in-seconds>"
+    exit 1
+fi
+
+TARGET=$1 
+INTERVAL=$2
+
+echo "Sending requests to $TARGET every $INTERVAL seconds."
+
+while true;do
+    REQUEST_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+    RESPONSE=$(curl -s "$TARGET")
+
+    echo "[$REQUEST_TIME] $RESPONSE"
+
+    sleep $INTERVAL
+done
